981e4e3dbafe65b45c062be0310e3964fbf87a67
chenchunpeng1994 <chenchunpeng1994@163.com>
2019-06-13 15:44:06 +0800

    sdk 1.0.0

--- huawenface/src/main/java/com/fpa/mainsupport/core/utils/VersionControler.java
+++ huawenface/src/main/java/com/fpa/mainsupport/core/utils/VersionControler.java
@@ -10,7 +10,6 @@ import android.support.v4.content.FileProvider;
 import android.util.Log;
 
 
-import com.huawen.huawenface.BuildConfig;
 
 import java.io.File;
 
@@ -22,25 +21,7 @@ public class VersionControler {
     static {
         //   sp = new SharedPreference(Constants.sp);
     }
-    // 安装程序
-    public static void install(Context context, String apkPath) {
-        Intent intent = new Intent(Intent.ACTION_VIEW);
-        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-        intent.setAction(Intent.ACTION_VIEW);
-        File apkFile=new File(apkPath);
 
-
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
-            intent.setFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
-            Uri contentUri = FileProvider.getUriForFile(context,
-                    BuildConfig.APPLICATION_ID + ".fileprovider", apkFile);
-            intent.setDataAndType(contentUri, "application/vnd.android.package-archive");
-        } else {
-            intent.setDataAndType(Uri.fromFile(apkFile), "application/vnd.android.package-archive");
-            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-        }
-        context.startActivity(intent);
-    }
 
 
     /**
